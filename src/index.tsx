c1fb09436c25deffc369d30e53fd5a921271d14e
liuwei89 <liuwei89@jd.com>
2018-05-07 11:43:21 +0800

    添加组件渲染性能监视工具

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,11 @@ import { BrowserRouter, Route } from "react-router-dom"
 import Index from "./components/index"
 import "./assets/style/base.css"
 
+if(process.env.NODE_ENV !== "production"){
+    const w = require("why-did-you-update");
+    w.whyDidYouUpdate(React)
+}
+
 const App = props => {
     return (
         <BrowserRouter basename="/">
